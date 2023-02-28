acdb7dcde2d136c4b7b63d25590d66eb0835865a
Yang Xu <xuyang@me.com>
2023-02-28 08:27:17 +0800

    add scrollViewDemo

---
+++ LayoutInTheSwiftUIWay/LayoutInTheSwiftUIWay/ScrollView.swift
@@ -0,0 +1,54 @@
+//
+//  ScrollView.swift
+//  LayoutInTheSwiftUIWay
+//
+//  Created by Yang Xu on 2023/2/28.
+//
+
+import Foundation
+import SwiftUI
+
+struct ScrollViewDemo: View {
+    @State var show = false
+    @State var screenSize: CGSize = .zero
+    @State var redViewSize: CGSize = .zero
+    var body: some View {
+        Color.clear
+            .overlay(
+                ScrollViewReader { proxy in
+                    ScrollView {
+                        VStack(spacing: 0) {
+                            Color.clear
+                                .frame(height: screenSize.height - redViewSize.height)
+                            RedView()
+                                .sizeInfo($redViewSize)
+                                .id("red")
+                            GreenView()
+                                .id("green")
+                        }
+                    }
+                    .onAppear {
+                        proxy.scrollTo("red", anchor: .bottom)
+                    }
+                    .onChange(of: show) { _ in
+                        withAnimation {
+                            if show {
+                                proxy.scrollTo("green", anchor: .bottom)
+                            } else {
+                                proxy.scrollTo("red", anchor: .bottom)
+                            }
+                        }
+                    }
+                }
+            )
+            .sizeInfo($screenSize)
+            .ignoresSafeArea()
+            .overlayButton(show: $show)
+    }
+}
+
+struct ScrollViewDemoPreview: PreviewProvider {
+    static var previews: some View {
+        ScrollViewDemo()
+    }
+}
