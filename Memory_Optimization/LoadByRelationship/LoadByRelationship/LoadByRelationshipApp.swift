05a33078fb731f0e952f0f67fa51467e810021af
fatbobman <xuyang@me.com>
2023-03-06 20:58:06 +0800

    add new project

---
+++ Memory_Optimization/LoadByRelationship/LoadByRelationship/LoadByRelationshipApp.swift
@@ -0,0 +1,20 @@
+//
+//  LoadByRelationshipApp.swift
+//  LoadByRelationship
+//
+//  Created by Yang Xu on 2023/3/6.
+//
+
+import SwiftUI
+
+@main
+struct LoadByRelationshipApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
