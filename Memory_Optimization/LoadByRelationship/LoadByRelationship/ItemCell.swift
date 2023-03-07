07a6fe7f4927ce463d7a8f5a14c4d844c3e4ae30
fatbobman <xuyang@me.com>
2023-03-07 10:25:25 +0800

    update

--- Memory_Optimization/LoadByRelationship/LoadByRelationship/ItemCell.swift
+++ Memory_Optimization/LoadByRelationship/LoadByRelationship/ItemCell.swift
@@ -12,19 +12,28 @@ struct ItemCell: View {
     @ObservedObject var item: Item
     @Environment(\.managedObjectContext) var viewContext
     let imageSize: CGSize = .init(width: 120, height: 160)
+    @State var show = true
     var body: some View {
         HStack {
-            Text(self.item.timestamp?.timeIntervalSince1970 ?? 0, format: .number)
-            if let data = item.picture?.data, let uiImage = UIImage(data: data), let image = Image(uiImage: uiImage) {
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: self.imageSize.width, height: self.imageSize.height)
+            if show {
+                Text(self.item.timestamp?.timeIntervalSince1970 ?? 0, format: .number)
+                if let data = item.picture?.data, let uiImage = UIImage(data: data), let image = Image(uiImage: uiImage) {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: self.imageSize.width, height: self.imageSize.height)
+                }
             }
         }
         .frame(minWidth: .zero, maxWidth: .infinity)
+        .onAppear {
+            show = true
+        }
         .onDisappear {
-//            viewContext.refresh(item, mergeChanges: false) // 并不会释放行缓存
+            show = false
+            if let picture = item.picture {
+                viewContext.refresh(picture, mergeChanges: false)
+            }
         }
     }
 }
--- Memory_Optimization/LoadByRelationship/LoadByRelationship/LoadByRelationshipApp.swift
+++ Memory_Optimization/LoadByRelationship/LoadByRelationship/LoadByRelationshipApp.swift
@@ -15,6 +15,99 @@ struct LoadByRelationshipApp: App {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//            TestView()
+//            MemeoryReleaseDemoByState()
+//            MemeoryReleaseDemoByStateObject()
         }
+    }
+}
+
+import CoreData
+
+// 测试.只适用于当 Picture 的 data 属性为外部存储
+struct TestView: View {
+    @State var pictures1 = [Picture]()
+    @State var pictures2 = [Picture]()
+    @State var memory: Float = 0
+    let fetchRequest: NSFetchRequest<Picture> = {
+        let fetchRequest = NSFetchRequest<Picture>(entityName: "Picture")
+        fetchRequest.sortDescriptors = []
+        fetchRequest.returnsObjectsAsFaults = false
+        return fetchRequest
+    }()
+
+    var body: some View {
+        VStack {
+            Text("Memory: \(memory, format: .number.precision(.fractionLength(2)))")
+            HStack {
+                Button("Load Picture1") {
+                    let context = PersistenceController.shared.container.viewContext
+                    pictures1 = (try? context.fetch(fetchRequest)) ?? []
+                    memory = reportMemory()
+                }
+                Button("Release") {
+                    pictures1 = []
+                }
+            }
+            HStack {
+                Button("Load Picture2") {
+                    let context = PersistenceController.shared.container.newBackgroundContext()
+                    context.performAndWait {
+                        pictures2 = (try? context.fetch(fetchRequest)) ?? []
+                    }
+                    memory = reportMemory()
+                }
+                Button("Release") {
+                    pictures2 = []
+                }
+            }
+        }
+        .buttonStyle(.borderedProminent)
+    }
+}
+
+struct MemeoryReleaseDemoByState: View {
+    @State var data: Data?
+    @State var memory: Float = 0
+    var body: some View {
+        VStack {
+            Text("memory :\(memory)")
+            Button("Generate Data") {
+                data = Data(repeating: 0, count: 10000000)
+                memory = reportMemory()
+            }
+            Button("ReleaseMemory") {
+                data = nil
+                memory = reportMemory()
+            }
+        }
+        .onAppear{
+            memory = reportMemory()
+        }
+    }
+}
+
+struct MemeoryReleaseDemoByStateObject: View {
+    @StateObject var holder = Holder()
+    @State var memory: Float = 0
+    var body: some View {
+        VStack {
+            Text("memory :\(memory)")
+            Button("Generate Data") {
+                holder.data = Data(repeating: 0, count: 10000000)
+                memory = reportMemory()
+            }
+            Button("ReleaseMemory") {
+                holder.data = nil
+                memory = reportMemory()
+            }
+        }
+        .onAppear{
+            memory = reportMemory()
+        }
+    }
+    
+    class Holder:ObservableObject {
+        @Published var data:Data?
     }
 }
